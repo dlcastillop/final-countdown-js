86a01572284a589c6c6d412011c8341b517cba81
Daniel Castillo <dlcastillo3015@gmail.com>
2023-07-16 14:58:28 -0400

    feat: add pause, play and reset functions to useCountUp

--- src/hooks/useCountUp.ts
+++ src/hooks/useCountUp.ts
@@ -1,13 +1,26 @@
 import { useState, useEffect } from "react";
 
-export const useCountUp = (min: number, max: number): number => {
+export const useCountUp = (
+  min: number,
+  max: number
+): {
+  current: number;
+  pause: () => void;
+  play: () => void;
+  reset: () => void;
+} => {
   if (min >= max) {
     throw new Error("The min parameter has to be less than the max parameter.");
   }
 
   const [count, setCount] = useState(min);
+  const [paused, setPaused] = useState(false);
 
   useEffect(() => {
+    if (paused) {
+      return;
+    }
+
     const interval = setInterval(() => {
       setCount((prev) => {
         return prev + 1;
@@ -20,7 +33,12 @@ export const useCountUp = (min: number, max: number): number => {
     }
 
     return () => clearInterval(interval);
-  }, [count, max]);
+  }, [count, max, paused]);
 
-  return count;
+  return {
+    current: count,
+    pause: () => setPaused(true),
+    play: () => setPaused(false),
+    reset: () => setCount(min),
+  };
 };
