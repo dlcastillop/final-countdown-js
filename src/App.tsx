ede306b94998807e6f6be5417741e29a199426be
Daniel Castillo <dlcastillo3015@gmail.com>
2023-07-23 08:57:59 -0400

    refactor: update package.json

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useTimer } from "./hooks";
+
+const App = () => {
+  const counter = useTimer(0, 0, 5);
+
+  return (
+    <>
+      <p>Counter value: {counter.current}</p>
+      <p>The counter is paused? {counter.isPaused ? "True" : "False"}</p>
+      <button onClick={() => counter.pause()}>Pause</button>
+      <button onClick={() => counter.play()}>Play</button>
+      <button onClick={() => counter.reset()}>Reset</button>
+    </>
+  );
+};
+
+export default App;
